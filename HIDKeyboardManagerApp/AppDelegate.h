0f1a80993bb669c215a8f2837bb7b277e8be26c4
Danil Korotenko <danil.korotenko@gmail.com>
2024-01-06 17:35:06 +0200

    app target added.

---
+++ HIDKeyboardManagerApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HIDKeyboardManagerApp
+//
+//  Created by Danil Korotenko on 1/6/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HIDKeyboardManagerApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HIDKeyboardManagerApp
+//
+//  Created by Danil Korotenko on 1/6/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
