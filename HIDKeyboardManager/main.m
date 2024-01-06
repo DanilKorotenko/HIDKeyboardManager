e4d4f22b73d5727f6007c72a4c432a063d59a5bd
Danil Korotenko <danil.korotenko@gmail.com>
2024-01-06 16:53:48 +0200

    Initial commit.

---
+++ HIDKeyboardManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HIDKeyboardManager
+//
+//  Created by Danil Korotenko on 1/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
